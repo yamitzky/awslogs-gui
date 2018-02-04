69d89ac5769ef67038a9206a5d4686502f4bf538
Yamitzky <negiga@gmail.com>
2018-02-04 17:58:57 +0900

    Env var for vue-router's history mode

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Logs from './Logs'
 import Streams from './Streams'
 
 export default new VueRouter({
+  mode: process.env.VUE_ROUTER_MODE || 'history',
   routes: [
     { path: '/', component: Groups },
     { path: '/streams', component: Streams },
